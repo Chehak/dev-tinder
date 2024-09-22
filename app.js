7906d495d303634f69308cbbcfde7147e4d3773d
chehak <gchehak18@gmail.com>
2024-09-22 22:56:10 +0530

    routing learning

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 
 app.listen(3000);
-app.arguments("/", (req, res) => {
-  res.send("Hello from the server");
+app.use("/test/2", (req, res) => {
+  res.send("Hello from the test2 server");
 });
+app.use("/test", (req, res) => {
+  res.send("Hello from the test server");
+});
+
+app.use("/xyz", (req, res) => {
+  res.send("Hello from the xyz");
+});
+
+app.get("/user", (req, res) => {
+  res.send({
+    id: req.query.id,
+    password: req.query.password,
+  });
+});
+
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//   res.send({
+//     id: req.params.userId,
+//     name: req.params.name,
+//     password: req.params.password,
+//   });
+// });
---
+++ readme.md
@@ -0,0 +1,20 @@
+Create a repository
+node_modules , package.json v/s package_lock.json 
+.bin folder inside node_modules 
+What are dependencies 
+what is the meaning of -g while npm install 
+Difference between caret ^ and tilde ~
+Insatll Express 
+Create a server in express 
+Listen to port 3000 
+Write request handler for '/hello' '/test'
+
+
+Order of writing the route matters a lot !! 
+Install postman 
+Play with it 
+Play with get post delete patch put api's 
+
+Routes can also be written with patterns like a?bc , ab+c ,ab*c , a(bc)+d  
+ regex link /a/, /.*fly$/  , you can create your own regex also
+ Read the query params in the routes and reading the dynamic routes

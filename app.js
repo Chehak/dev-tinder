eb7b7107076bf87f27e7c13e221bd04e66eaab60
chehak <gchehak18@gmail.com>
2024-09-26 21:49:43 +0530

    error-handling, using wildcard route and try-catch

--- app.js
+++ app.js
@@ -10,8 +10,34 @@ app.use("/user/login", (req, res) => {
   res.send("User login api");
 });
 
-app.use("/user", userAuth, (req, res, next) => {
-  res.send("hello from user api");
+// error is not there on first then it will not run
+
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status(500).send("Something went wrong");
+//   }
+// });
+
+//another one is we can use try catch so that, is is the best practice of handling errors , generally try catch is used
+//errors are handled within the route
+app.use("/user", (req, res, next) => {
+  // throw new Error("jjhjhjhjj");
+  // res.send("hello from user api");
+  try {
+    throw new Error("jjhjhjhjj");
+    res.send("hello from user api");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//one method of error handling is that we can add a wildcard route so that if any of the route has some error
+//then this route will call and handles error efficiently
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(3000);
@@ -23,3 +49,5 @@ app.listen(3000);
 //if suppose i had created multiple route for login and i dont want to authenticate my user/login route then what i can do
 // is to just add the userAuth to the /user route and make the /user/login to the top so that it dont have to
 //go through the user route simple
+
+//ORDER OF ROUTES MATTERS A LOT !!!
